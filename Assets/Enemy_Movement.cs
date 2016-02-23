0da42e5aa5a0b184a043f0f673bf28c21a9f37e6
huawillian <huawillian@gmail.com>
2016-02-23 13:45:12 -0500

    First commit for Terrain feature plugin.

---
+++ Assets/Enemy_Movement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Movement : MonoBehaviour
+{
+	public NavMeshAgent agent;
+	public GameObject person;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		agent.destination = person.transform.position; //transform of the person object
+	}
+}
---
+++ Assets/Person_Movement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Person_Movement : MonoBehaviour
+{
+	public Rigidbody rigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		Vector3 vel = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.W)) {
+			print ("W is held down");
+			vel += new Vector3 (0,0,2);
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			print ("A is held down");
+			vel += new Vector3 (-2,0,0);
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			print ("S is held down");
+			vel += new Vector3 (0,0,-2);
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			print ("D is held down");
+			vel += new Vector3 (2,0,0);
+		}
+
+		rigidbody.velocity = vel;
+	}
+}
